68ab892a2c1021b1956d5d3671113dd67aa84cb0
Natty Teshome <nattyteshome30@gmail.com>
2022-07-13 17:49:37 -0500

    Test template

---
+++ src/test/java/FunctionUtilTest.java
@@ -0,0 +1,35 @@
+import Model.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FunctionUtilTest {
+
+    List<Account> accounts;
+    List<Address> addresses;
+    List<AppUser> admins;
+    List<AppUser> guests;
+    List<AppUser> hosts;
+    List<Country> countries;
+    List<HomeProperty> homeProperties;
+    List<Property> properties;
+    List<Reservation> reservations;
+    List<City> cities;
+    List<Payment> payments;
+
+
+
+    @Before
+    public void setUp(){
+
+
+
+    }
+
+    @Test
+    public void testFun1(){
+
+    }
+
+}
