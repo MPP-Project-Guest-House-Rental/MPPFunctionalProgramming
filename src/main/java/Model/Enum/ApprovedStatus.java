0adbcc41c92711c9c92b01d5c085e46c0a75085a
wende <wendmagegn.b@gmail.com>
2022-07-13 20:09:10 -0500

    Factory Method

--- src/main/java/Model/Enum/ApprovedStatus.java
+++ src/main/java/Model/Enum/ApprovedStatus.java
@@ -1,5 +1,16 @@
 package Model.Enum;
 
+import java.util.List;
+import java.util.Random;
+
 public enum ApprovedStatus {
-    APPROVED, DISAPPROVED, PENDING
+    APPROVED, DISAPPROVED, PENDING;
+    private static final List<ApprovedStatus> VALUES =
+            List.of(values());
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static ApprovedStatus status()  {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
 }
